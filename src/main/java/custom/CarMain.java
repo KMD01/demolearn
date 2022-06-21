6e2979eb7d4ac471b7c3fdbd3184886e52c93169
Krzysiek <dyk.krzysztof@gmail.com>
2022-06-21 06:28:34 +0200

    Some other changes2

--- src/main/java/custom/CarMain.java
+++ src/main/java/custom/CarMain.java
@@ -1,15 +1,25 @@
 package custom;
 
 
+import java.util.List;
+
 public class CarMain {
 
     public static void main(String[] args) {
 
-        Car car1 = new Car();
-        car1.printCarParameters();
-        Car car2 = new Car(250,"Ford");
-        car2.printCarParameters();
-        Car car3 = new Car("Red", 320, "Ferrari");
-        car3.printCarParameters();
+        List<Car> carsList = CarService.carGenerator(10);
+        boolean isListFull = !carsList.isEmpty();
+        System.out.println("Czy lista ma elementy: " + isListFull);
+        for (Car car:carsList) {
+            System.out.println(car.getBrand());
+        }
+
+
+        //Car car1 = new Car();
+       // car1.printCarParameters();
+        //Car car2 = new Car(250,"Ford");
+        //car2.printCarParameters();
+       // Car car3 = new Car("Red", 320, "Ferrari");
+        //car3.printCarParameters();
     }
 }
