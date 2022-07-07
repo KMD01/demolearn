82b25c321ac9f98bda2c9f16f8dab3e3feb5470b
Krzysiek <dyk.krzysztof@gmail.com>
2022-07-07 19:26:26 +0200

    Calculator from BRO

--- src/main/java/methods/TimeMethod.java
+++ src/main/java/methods/TimeMethod.java
@@ -7,19 +7,8 @@ import java.time.temporal.TemporalAmount;
 
 
 public class TimeMethod {
-
-
-
     public void showDuration(LocalDate start, LocalDate end){
         Duration periodOfVacations = Duration.between(start,end);
     }
-
-
-
     Duration d1 = Duration.ofDays(5);
-
-
-
-
-
 }
