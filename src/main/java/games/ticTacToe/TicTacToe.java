ba1b5e6306c9092d0d03f9c0281146eaacdb3af9
KrzysztofDyk <krzysztof.dyk@jit.team>
2022-07-23 20:34:45 +0200

    TicTacToe Counter added.

--- src/main/java/games/ticTacToe/TicTacToe.java
+++ src/main/java/games/ticTacToe/TicTacToe.java
@@ -11,6 +11,8 @@ public class TicTacToe implements ActionListener {
 
     boolean turn = true;
     int step = 0;
+    int player1Win =0;
+    int player2Win =0;
 
     JFrame jFrame;
     JLabel jLabel1, jLabel2;
@@ -25,6 +27,20 @@ public class TicTacToe implements ActionListener {
     JColorChooser jColorChooser;
     Color winColor = new Color(255,215,0);
     Color backgroundColor = new Color(189,183,107);
+
+    public int countPlayer1Wins(){
+        return player1Win;
+    };
+    public  int countPlayer2Wins(){
+        return player2Win;
+    };
+
+    public void refreshScore(){
+        String label1 ="PLAYER 'X': "+ String.valueOf(countPlayer1Wins());
+        String label2 ="PLAYER 'O': "+ String.valueOf(countPlayer2Wins());
+        jLabel1.setText(label1);
+        jLabel2.setText(label2);
+    }
 
     public void play() {
 
@@ -62,10 +78,12 @@ public class TicTacToe implements ActionListener {
         jMenuFile.add(mExit);
         mExit.addActionListener(this);
 
-        jLabel1 = new JLabel("PLAYER 'X': ");
+        jLabel1 = new JLabel();
+        String label1 = "PLAYER 'X': "+ String.valueOf(countPlayer1Wins());
+        jLabel1.setText(label1);
         jLabel1.setFont(new Font("Verdana", Font.BOLD, 20));
 
-        jLabel2 = new JLabel("PLAYER 'O': ");
+        jLabel2 = new JLabel("PLAYER 'O': " + String.valueOf(countPlayer2Wins()));
         jLabel2.setFont(new Font("Verdana", Font.BOLD, 20));
 
         jPanel1 = new JPanel();
@@ -215,6 +233,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] row2 = {3, 4, 5};
         if (jButtons[row2[0]].getText() == "X" && jButtons[row2[1]].getText() == "X" && jButtons[row2[2]].getText() == "X") {
@@ -224,6 +244,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] row3 = {6, 7, 8};
         if (jButtons[row3[0]].getText() == "X" && jButtons[row3[1]].getText() == "X" && jButtons[row3[2]].getText() == "X") {
@@ -233,6 +255,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] column1 = {0, 3, 6};
         if (jButtons[column1[0]].getText() == "X" && jButtons[column1[1]].getText() == "X" && jButtons[column1[2]].getText() == "X") {
@@ -242,6 +266,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] column2 = {1, 4, 7};
         if (jButtons[column2[0]].getText() == "X" && jButtons[column2[1]].getText() == "X" && jButtons[column2[2]].getText() == "X") {
@@ -251,6 +277,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] column3 = {2, 5, 8};
         if (jButtons[column3[0]].getText() == "X" && jButtons[column3[1]].getText() == "X" && jButtons[column3[2]].getText() == "X") {
@@ -260,6 +288,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] diagonal1 = {0, 4, 8};
         if (jButtons[diagonal1[0]].getText() == "X" && jButtons[diagonal1[1]].getText() == "X" && jButtons[diagonal1[2]].getText() == "X") {
@@ -269,6 +299,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }
         int[] diagonal2 = {2, 4, 6};
         if (jButtons[diagonal2[0]].getText() == "X" && jButtons[diagonal2[1]].getText() == "X" && jButtons[diagonal2[2]].getText() == "X") {
@@ -278,6 +310,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player1Win++;
+            refreshScore();
         }        
     }
     private void check_O(Color color) {
@@ -289,6 +323,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] row2 = {3, 4, 5};
         if (jButtons[row2[0]].getText() == "O" && jButtons[row2[1]].getText() == "O" && jButtons[row2[2]].getText() == "O") {
@@ -298,6 +334,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] row3 = {6, 7, 8};
         if (jButtons[row3[0]].getText() == "O" && jButtons[row3[1]].getText() == "O" && jButtons[row3[2]].getText() == "O") {
@@ -307,6 +345,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] column1 = {0, 3, 6};
         if (jButtons[column1[0]].getText() == "O" && jButtons[column1[1]].getText() == "O" && jButtons[column1[2]].getText() == "O") {
@@ -316,6 +356,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] column2 = {1, 4, 7};
         if (jButtons[column2[0]].getText() == "O" && jButtons[column2[1]].getText() == "O" && jButtons[column2[2]].getText() == "O") {
@@ -325,6 +367,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] column3 = {2, 5, 8};
         if (jButtons[column3[0]].getText() == "O" && jButtons[column3[1]].getText() == "O" && jButtons[column3[2]].getText() == "O") {
@@ -334,6 +378,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] diagonal1 = {0, 4, 8};
         if (jButtons[diagonal1[0]].getText() == "O" && jButtons[diagonal1[1]].getText() == "O" && jButtons[diagonal1[2]].getText() == "O") {
@@ -343,6 +389,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
         int[] diagonal2 = {2, 4, 6};
         if (jButtons[diagonal2[0]].getText() == "O" && jButtons[diagonal2[1]].getText() == "O" && jButtons[diagonal2[2]].getText() == "O") {
@@ -352,6 +400,8 @@ public class TicTacToe implements ActionListener {
             for (int i = 0; i < 9; i++) {
                 jButtons[i].setEnabled(false);
             }
+            player2Win++;
+            refreshScore();
         }
     }
 }
