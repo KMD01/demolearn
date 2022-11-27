1f7bec53540d47f8347e1fb6eede5369bd845550
kDyk <dyk.krzysztof@gmail.com>
2022-11-27 21:23:39 +0100

    feat. switch (->) java14

---
+++ src/main/java/steringInstructions/SteringMain.java
@@ -0,0 +1,11 @@
+package steringInstructions;
+
+public class SteringMain {
+
+    public static void main(String[] args) {
+
+        Stering stering = new Stering();
+        stering.printTheMonth(12);
+
+    }
+}
