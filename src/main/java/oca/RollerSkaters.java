cdd0b6228289b3cdd759c129c5624550e022d1b5
kDyk <dyk.krzysztof@gmail.com>
2022-12-04 09:30:54 +0100

    feat. oca

---
+++ src/main/java/oca/RollerSkaters.java
@@ -0,0 +1,15 @@
+package oca;
+
+public class RollerSkaters {
+
+    static int wheels = 1;
+    int tracks = 5;
+
+    public static void main(String[] args) {
+        RollerSkaters s = new RollerSkaters();
+        int feet =4, tracks =15;
+        System.out.println(feet + tracks + s.wheels);
+
+    }
+
+}
